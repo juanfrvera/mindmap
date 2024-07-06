697457fc5f709946496bcb1a4f82ea852d454ff7
Juan <juanfrvera@gmail.com>
2024-07-06 12:25:17 -0300

    fix: detect clicks on canvas

--- src/app/feat/map/map.tsx
+++ src/app/feat/map/map.tsx
@@ -6,7 +6,7 @@ import { iItem } from "../item/typings";
 import { IPosition } from "../position/position";
 
 export default function Map() {
-  const clickOnContainer = (ev: React.MouseEvent<HTMLDivElement>) => {
+  const clickOnSvgCanvas = (ev: React.MouseEvent<SVGElement>) => {
     addItem({ x: ev.clientX, y: ev.clientY });
   };
 
@@ -20,8 +20,10 @@ export default function Map() {
   const itemsUi = items.map((item) => <Item key={item.id} item={item}></Item>);
 
   return (
-    <div onClick={clickOnContainer} className={styles.mapContainer}>
-      <svg className={styles.svgCanvas}>{itemsUi}</svg>
+    <div className={styles.mapContainer}>
+      <svg onClick={clickOnSvgCanvas} className={styles.svgCanvas}>
+        {itemsUi}
+      </svg>
       <div className={styles.commandBarContainer}>
         <Command></Command>
       </div>
