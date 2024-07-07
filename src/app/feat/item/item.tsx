b462a46321b66c9fdabd6ae842caf72cf025776b
Juan <juanfrvera@gmail.com>
2024-07-07 10:47:51 -0300

    feat: docus text container after creating an item

--- src/app/feat/item/item.tsx
+++ src/app/feat/item/item.tsx
@@ -1,6 +1,6 @@
 import { iItem } from "./typings";
 import styles from "./item.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Item({ item }: { item: iItem }) {
   const [editing, setEditing] = useState(true);
@@ -9,15 +9,25 @@ export default function Item({ item }: { item: iItem }) {
     ev.stopPropagation();
   };
 
+  // I want to use an effect to trigger at the first render
+  // and focus on the text container
+  useEffect(() => {
+    // I want to focus on the text container
+    document.getElementById(`textContainer_${item.id}`)?.focus();
+  }, [item.id]);
+
   return (
     <div
       onClick={clickOnContainer}
       className={styles.container}
       style={{ left: item.position.x, top: item.position.y }}
     >
-      <div className={styles.textContainer} contentEditable={editing} autoFocus>
-        Here goes your text
-      </div>
+      <div
+        id={`textContainer_${item.id}`}
+        className={styles.textContainer}
+        contentEditable={editing}
+        autoFocus
+      ></div>
     </div>
   );
 }
